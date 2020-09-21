b3230d12198f1fc0a2f5976f29edb00b242b1029
AncutaBiro <ancuta.biro@yahoo.com>
2020-09-21 21:23:38 +0300

    Add project files.

---
+++ OperatorsExpressions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OperatorsExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((char)99);
+            Console.Read();
+        }
+    }
+}
